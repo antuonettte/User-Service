b971949dcb1c033b4a3b365d7f15595b5ee73999
Antonio Turner <tonyturner1399@gmail.com>
2024-05-13 09:14:47 -0400

    testing push

--- lambdafunction/lambda_function.py
+++ lambdafunction/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # successful deployment
+    # successful deployment done
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
