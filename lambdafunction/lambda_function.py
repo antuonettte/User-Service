0a127cd01cce7c42952adddb7384bdae68f92d8e
Antonio Turner <tonyturner1399@gmail.com>
2024-06-12 18:03:41 -0400

    updating

--- lambdafunction/lambda_function.py
+++ lambdafunction/lambda_function.py
@@ -66,10 +66,13 @@ def lambda_handler(event, context):
         if http_method == 'GET':
             if path == '/user-management/user':
                 logger.info('Getting User by ID')
-                return get_user_by_id()
+                
+                id = query_parameters.get('id')
+                
+                return get_user_by_id(id)
             elif path == '/user-management/users':
                 logger.info("Getting all users")
-                return get_all_users(query_parameters)
+                return get_all_users(event)
         
         elif http_method == 'POST':
             
@@ -113,7 +116,7 @@ def get_user_by_id(user_id):
     
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT id, username, email, first_name, last_name, profile_picture, bio, location, dob, phone_number, status, follower_count, following_count FROM users WHERE id = %s"
+            sql = "SELECT id, username, email, first_name, last_name, profile_picture_url, bio, location, date_of_birth, phone_number, status, follower_count, following_count FROM users WHERE id = %s"
             cursor.execute(sql, (user_id,))
             result = cursor.fetchone()
             
@@ -171,7 +174,7 @@ def get_post_ids_for_user(user_id, domain_endpoint):
     if response.status_code == 200:
         search_results = response.json()
         post_ids = [hit["_source"]["id"] for hit in search_results["hits"]["hits"]]
-        return post_ids
+        return tuple(post_ids)
     else:
         raise Exception(f"Error: {response.status_code}, {response.text}")
     
@@ -197,7 +200,7 @@ def delete_user_data(user_id):
     # Delete comments related to user's posts
     delete_comments_sql = '''
     DELETE FROM comments
-    WHERE post_id IN (SELECT id FROM posts WHERE user_id = %s)
+    WHERE post_id IN %s
     '''
     
     user_conn = pymysql.connect(
@@ -222,19 +225,28 @@ def delete_user_data(user_id):
     )
 
     try:
+        
+        logger.info("Getting Post ID's")
+        post_ids = get_post_ids_for_user(user_id, DOMAIN_ENDPOINT)
+        logger.info(post_ids)
+        
         # Delete comments
+        logger.info("Deleting Comments")
         with comments_conn.cursor() as cursor:
-            cursor.execute(delete_comments_sql, (user_id,))
+            cursor.execute(delete_comments_sql, ( post_ids, ))
         
         # Delete posts
+        logger.info("Deleting Posts")
         with posts_conn.cursor() as cursor:
             cursor.execute(delete_posts_sql, (user_id,))
         
         # Delete follower relationships
+        logger.info("Deleting Follower Relationships")
         with user_conn.cursor() as cursor:
             cursor.execute(delete_follower_sql, (user_id, user_id))
         
         # Delete user
+        logger.info("Deleting User")
         with user_conn.cursor() as cursor:
             cursor.execute(delete_user_sql, (user_id,))
         
@@ -243,6 +255,11 @@ def delete_user_data(user_id):
         user_conn.commit()
         posts_conn.commit()
         comments_conn.commit()
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'User Successfully Deleted'})
+        }
         
     except Exception as e:
         user_conn.rollback()
@@ -255,38 +272,82 @@ def delete_user_data(user_id):
         posts_conn.close()
         comments_conn.close()
 
-def get_all_users(query_parameters):
-    conn = pymysql.connect(
+def get_all_users(event):
+    connection = pymysql.connect(
         host=USER_DB_HOST,
         user=USER_DB_USER,
         password=USER_DB_PASSWORD,
         database=USER_DB_NAME
-    )
-
+        )
+                                     
     try:
-        page = int(query_parameters.get('page', 1))  # Default to page 1
-        limit = int(query_parameters.get('limit', 10))  # Default to 10 items per page
-        offset = (page - 1)*limit
+        page = 1
+        limit = 10
+        
+        query_parameters = event.get('queryStringParameters', {})
+        if query_parameters:
+            if 'page' in query_parameters:
+                try:
+                    page = int(query_parameters['page'])
+                except ValueError:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps({'error': 'Page must be an integer'})
+                    }
+            if 'limit' in query_parameters:
+                try:
+                    limit = int(query_parameters['limit'])
+                except ValueError:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps({'error': 'Limit must be an integer'})
+                    }
+                
+        offset = (page - 1) * limit
 
-        logger.info("getting all users")
-        with conn.cursor() as cursor:
+        
+        
+        with connection.cursor() as cursor:
             sql = "SELECT * FROM users LIMIT %s OFFSET %s"
             cursor.execute(sql, (limit, offset))
             users = cursor.fetchall()
+            
+            user_list = []
+            
+            if users:
+                for user in users:
+                    user_dict = {
+                        'id': user[0],
+                        'username': user[1],
+                        'email': user[2],
+                        'createdAt': user[3].strftime('%Y-%m-%d %H:%M:%S'),
+                        'updatedAt': user[4].strftime('%Y-%m-%d %H:%M:%S'),
+                        'first_name': user[5],
+                        'last_name': user[6],
+                        'profile_picture': user[7],
+                        'bio': user[8],
+                        'location': user[9],
+                        'dob': user[10],
+                        'phone_number': user[11],
+                        'status': user[12],
+                        'follower_count': user[13],
+                        'following_count': user[14]
+                    }
+                    
+                    user_list.append(user_dict)
         
         return {
             'statusCode': 200,
-            'body': json.dumps({'users': users})
+            'body': json.dumps({'users': user_list})
         }
 
     except Exception as e:
-        logger.error(str(e))
         return {
             'statusCode': 500,
-            'body': json.dumps({'erre': str(e)})
+            'body': json.dumps({'error': str(e)})
         }
     finally:
-        conn.close()
+        connection.close()
 
 def create_follow_relationship(user_id, follower_id):
     conn = pymysql.connect(
@@ -332,65 +393,142 @@ def check_follow_relationship(user_id, follower_id, cursor):
     return result[0] > 0
 
 def create_user(user_data):
-    # Extract user data from request body
-    if ('username' not in user_data) or ('email' not in user_data) or ('first_name' not in user_data) or ('last_name' not in user_data):
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Please Enter Required Fields'}) 
-        }
-
-    username = user_data.get('username')
-    email = user_data.get('email')
-    first_name = user_data.get('first_name')
-    last_name = user_data.get('last_name')
-    profile_picture = user_data.get('profile_picture', None)
-    bio = user_data.get('bio', None)
-    location = user_data.get('location', None)
-    dob = user_data.get('dob', 'null')
-    phone_number = user_data.get('phone_number', None)
-    status = user_data.get('status', 'active')
-    follower_count = user_data.get('follower_count', 0)
-    following_count = user_data.get('following_count', 0)
-    
-    # Connect to the database
-    conn = pymysql.connect(
+    connection = pymysql.connect(
         host=USER_DB_HOST,
         user=USER_DB_USER,
         password=USER_DB_PASSWORD,
         database=USER_DB_NAME
-    )
-    
+        )
     try:
-        logger.info("Inserting user into db")
-        with conn.cursor() as cursor:
-            # SQL statement to insert user into users table
-            sql = '''
-            INSERT INTO users (username, email, first_name, last_name, profile_picture_url, bio, location, date_of_birth, phone_number, status, follower_count, following_count)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            '''
-            # Execute the SQL statement
-            cursor.execute(sql, (username, email, first_name, last_name, profile_picture, bio, location, dob, phone_number, status, follower_count, following_count))
+        logger.info("Creating user")
+        logger.info(user_data)
+
+        # Validate required fields
+        logger.info("Validating Fields")
+        required_fields = ['username', 'email', 'first_name']
+        for field in required_fields:
+            if field not in user_data:
+                return {
+                    'statusCode': 400,
+                    'body': json.dumps({'error': f'Missing required field: {field}'})
+                }
+
+        # Set default values for optional fields
+        optional_fields = [
+            'last_name', 'profile_picture', 'bio', 'location', 'dob',
+            'phone_number', 'status', 'follower_count', 'following_count'
+        ]
+        for field in optional_fields:
+            if field not in user_data:
+                user_data[field] = None
+
+        # Default values for follower_count and following_count
+        if user_data['follower_count'] is None:
+            user_data['follower_count'] = 0
+        if user_data['following_count'] is None:
+            user_data['following_count'] = 0
+
+        # Set status to 'active' if not provided
+        if user_data['status'] is None:
+            user_data['status'] = 'active'
+
         
-        # Commit changes to the database
-        conn.commit()
+
         
-        # Return success response
+        logger.info("Inserting into db")
+        with connection.cursor() as cursor:
+            sql = """
+            INSERT INTO users (
+                username, email, first_name, last_name,
+                profile_picture_url, bio, location, date_of_birth, phone_number, status,
+                follower_count, following_count
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(sql, (
+                user_data['username'], user_data['email'],
+                user_data['first_name'], user_data['last_name'], user_data['profile_picture'],
+                user_data['bio'], user_data['location'], user_data['dob'], user_data['phone_number'],
+                user_data['status'], user_data['follower_count'], user_data['following_count']
+            ))
+            connection.commit()
+            
+            # Retrieve the ID of the newly created user
+            user_id = cursor.lastrowid
+
         return {
             'statusCode': 201,
-            'body': json.dumps({'message': 'User created successfully'})
+            'body': json.dumps({'message': 'User created successfully', 'user_id': user_id})
         }
-    
+
     except Exception as e:
-        logger.error(f"Error creating user: {str(e)}")
-        # Rollback the transaction in case of error
-        conn.rollback()
-        
-        # Return error response
+        connection.rollback()
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': 'Failed to create user'})
+            'body': json.dumps({'error': str(e)})
         }
-    
     finally:
-        # Close database connection
-        conn.close()
+        connection.close()
+
+# def create_user(user_data):
+#     # Extract user data from request body
+#     if ('username' not in user_data) or ('email' not in user_data) or ('first_name' not in user_data) or ('last_name' not in user_data):
+#         return {
+#             'statusCode': 400,
+#             'body': json.dumps({'error': 'Please Enter Required Fields'}) 
+#         }
+
+#     username = user_data.get('username')
+#     email = user_data.get('email')
+#     first_name = user_data.get('first_name')
+#     last_name = user_data.get('last_name')
+#     profile_picture = user_data.get('profile_picture', None)
+#     bio = user_data.get('bio', None)
+#     location = user_data.get('location', None)
+#     dob = user_data.get('dob', 'null')
+#     phone_number = user_data.get('phone_number', None)
+#     status = user_data.get('status', 'active')
+#     follower_count = user_data.get('follower_count', 0)
+#     following_count = user_data.get('following_count', 0)
+    
+#     # Connect to the database
+#     conn = pymysql.connect(
+#         host=USER_DB_HOST,
+#         user=USER_DB_USER,
+#         password=USER_DB_PASSWORD,
+#         database=USER_DB_NAME
+#     )
+    
+#     try:
+#         logger.info("Inserting user into db")
+#         with conn.cursor() as cursor:
+#             # SQL statement to insert user into users table
+#             sql = '''
+#             INSERT INTO users (username, email, first_name, last_name, profile_picture_url, bio, location, date_of_birth, phone_number, status, follower_count, following_count)
+#             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+#             '''
+#             # Execute the SQL statement
+#             cursor.execute(sql, (username, email, first_name, last_name, profile_picture, bio, location, dob, phone_number, status, follower_count, following_count))
+        
+#         # Commit changes to the database
+#         conn.commit()
+        
+#         # Return success response
+#         return {
+#             'statusCode': 201,
+#             'body': json.dumps({'message': 'User created successfully'})
+#         }
+    
+#     except Exception as e:
+#         logger.error(f"Error creating user: {str(e)}")
+#         # Rollback the transaction in case of error
+#         conn.rollback()
+        
+#         # Return error response
+#         return {
+#             'statusCode': 500,
+#             'body': json.dumps({'error': 'Failed to create user'})
+#         }
+    
+#     finally:
+#         # Close database connection
+#         conn.close()
